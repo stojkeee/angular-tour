649512e96e0587282d8687e04934ae9ed6bf64ae
stojic.mate <stojic.mate@nsoft.com>
2022-12-10 21:32:24 +0100

    create product wrapper

--- src/app/modules/webshop/pages/products/products.component.ts
+++ src/app/modules/webshop/pages/products/products.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductCardComponent } from '../../components/product-card/product-card.component';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ProductCardComponent],
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss'],
 })
