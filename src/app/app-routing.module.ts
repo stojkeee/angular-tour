35e64e146273b714eaee6c4189e7d2f553e32424
stojic.mate <stojic.mate@nsoft.com>
2022-12-10 18:18:52 +0100

    separate routes into modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/webshop/router').then(mod => mod.WEBSHOP_ROUTES),
+  },
+  {
+    path: 'backoffice',
+    loadChildren: () =>
+      import('./modules/back-office/router').then(mod => mod.BACKOFFICE_ROUTES),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
