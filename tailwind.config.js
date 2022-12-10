ce866fae9be89a4a32e5038367a9ea26dad21713
stojic.mate <stojic.mate@nsoft.com>
2022-12-10 18:18:20 +0100

    add basic color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      yellow: '#f8ff1d',
+      black: '#000a00',
+      green: '#50ffaf',
+      blue: '#4b69fe',
+      red: '#ff4c4b',
+      pink: '#ff99d9',
+      white: '#ffffff',
+    },
   },
   plugins: [],
 };
