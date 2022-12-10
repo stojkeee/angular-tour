aacc68ee17178d9fbac3dde221d23b5bb7d037c1
stojic.mate <stojic.mate@nsoft.com>
2022-12-10 18:13:26 +0100

    create chart component

---
+++ src/app/modules/webshop/pages/chart/chart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-chart',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './chart.component.html',
+  styleUrls: ['./chart.component.scss'],
+})
+export class ChartComponent {}
