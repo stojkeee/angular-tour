3d67ad28fe35fac162058ad832b233e8bc70f275
stojic.mate <stojic.mate@nsoft.com>
2022-12-10 18:18:00 +0100

    create back office router

---
+++ src/app/modules/back-office/router.ts
@@ -0,0 +1,10 @@
+import { Route } from '@angular/router';
+
+import { BackOfficeComponent } from './back-office.component';
+
+export const BACKOFFICE_ROUTES: Route[] = [
+  {
+    path: '',
+    component: BackOfficeComponent,
+  },
+];
