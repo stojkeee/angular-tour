aa89061e82993541953309a4aec90c485b03bd67
stojic.mate <stojic.mate@nsoft.com>
2022-12-10 18:19:03 +0100

    create back office component

---
+++ src/app/modules/back-office/back-office.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-back-office',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './back-office.component.html',
+  styleUrls: ['./back-office.component.scss'],
+})
+export class BackOfficeComponent {}
