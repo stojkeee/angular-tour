dc1a2f26a56060e3bad961bd30beb0e2037d1c76
stojic.mate <stojic.mate@nsoft.com>
2022-12-15 14:42:32 +0100

    add http client module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppRoutingModule } from './app/app-routing.module';
 import { AppComponent } from './app/app.component';
+import { environment } from './environment/environment.prod';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(AppRoutingModule)],
+  providers: [
+    importProvidersFrom(AppRoutingModule),
+    importProvidersFrom(HttpClientModule),
+  ],
 });
