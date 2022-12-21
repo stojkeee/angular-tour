0632e234893eab0cc36fcf7abb3e20e9f32eee50
stojic.mate <stojic.mate@nsoft.com>
2022-12-21 22:01:46 +0100

    add get jackpot logic

--- src/app/modules/webshop/pages/cart.component.ts
+++ src/app/modules/webshop/pages/cart.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CartService } from '../services/cart.service';
+import { Observable, map, tap } from 'rxjs';
+import { CartItem } from '../models/cart-item.interface';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
   imports: [CommonModule],
-  template: `<p>cart works!</p> `,
+  template: `
+    <ng-container *ngIf="cartItems$ | async as items">
+      {{ items | json }}
+    </ng-container>
+  `,
   styles: [``],
 })
-export class CartComponent {}
+export class CartComponent implements OnInit {
+  cartItems$!: Observable<CartItem[]>;
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.cartItems$ = this.cartService.cartItems$.pipe(
+      map(items =>
+        items.reduce((acc: CartItem[], curr) => {
+          const objInAcc = acc.find(
+            (o: CartItem) => o.product === curr.product
+          );
+          if (objInAcc) objInAcc.quantity += curr.quantity;
+          else acc.push(curr);
+          return acc;
+        }, [])
+      )
+    );
+  }
+}
