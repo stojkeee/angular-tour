53183cabb998a341306acbc8d76afcde9142bd64
stojic.mate <stojic.mate@nsoft.com>
2023-01-02 20:42:48 +0100

    add and remove cart state logic

--- src/app/modules/webshop/services/cart.service.ts
+++ src/app/modules/webshop/services/cart.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { CartItem } from '../models/cart-item.interface';
+import { Product } from '../models/product.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -8,17 +9,38 @@ import { CartItem } from '../models/cart-item.interface';
 export class CartService {
   private readonly _cartItems = new BehaviorSubject<CartItem[]>([]);
 
-  cartItems$: Observable<CartItem[]> = this._cartItems.asObservable();
+  readonly cartItems$: Observable<CartItem[]> = this._cartItems.asObservable();
 
   get cartItems(): CartItem[] {
     return this._cartItems.getValue();
   }
 
-  private set cartItems(item: CartItem[]) {
+  private set setCartItems(item: CartItem[]) {
     this._cartItems.next(item);
   }
 
-  addToCart(item: CartItem) {
-    this.cartItems = [...this.cartItems, item];
+  addToCart(product: Product) {
+    const index = this.cartItems.findIndex(
+      item => item.product.id === product.id
+    );
+    if (index < 0) {
+      this.setCartItems = [...this.cartItems, { product, quantity: 1 }];
+    } else {
+      this.cartItems[index].quantity += 1;
+      this.setCartItems = [...this.cartItems];
+    }
+  }
+
+  removeFromCart(product: Product) {
+    const index = this.cartItems.findIndex(
+      item => item.product.id === product.id
+    );
+    if (this.cartItems[index].quantity <= 1) {
+      this.cartItems.splice(index, 1);
+      this.setCartItems = [...this.cartItems];
+    } else {
+      this.cartItems[index].quantity -= 1;
+      this.setCartItems = [...this.cartItems];
+    }
   }
 }
