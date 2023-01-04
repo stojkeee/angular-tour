488f3b33ccc7b133381459fe87e471035bda70d0
stojic.mate <stojic.mate@nsoft.com>
2023-01-04 20:49:45 +0100

    add http interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppRoutingModule } from './app/app-routing.module';
@@ -6,6 +6,7 @@ import { AppComponent } from './app/app.component';
 import { environment } from './environment/environment.prod';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { ResReqInterceptor } from './app/core/res-req.interceptor';
 
 if (environment.production) {
   enableProdMode();
@@ -21,6 +22,11 @@ bootstrapApplication(AppComponent, {
     {
       provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
       useValue: { appearance: 'outline', class: 'w-full' },
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ResReqInterceptor,
+      multi: true,
     },
   ],
 });
