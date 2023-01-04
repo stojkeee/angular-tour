54284d9f80206341bd3ca0a722aec8e7895a1727
stojic.mate <stojic.mate@nsoft.com>
2023-01-04 20:50:05 +0100

    use loaders

--- src/app/modules/webshop/pages/products.component.ts
+++ src/app/modules/webshop/pages/products.component.ts
@@ -5,11 +5,18 @@ import { Observable } from 'rxjs';
 import { Product } from '../models/product.interface';
 import { ProductsService } from '../services/products.service';
 import { ActivatedRoute, RouterModule } from '@angular/router';
+import { LoaderService } from 'src/app/core/loader.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule, ProductCardComponent, RouterModule],
+  imports: [
+    CommonModule,
+    ProductCardComponent,
+    RouterModule,
+    MatProgressSpinnerModule,
+  ],
   template: `
     <div class="bg-purple h-[calc(100vh-49px)] overflow-y-auto">
       <div class="container mx-auto px-2 md:px-0">
@@ -17,11 +24,20 @@ import { ActivatedRoute, RouterModule } from '@angular/router';
           class="md:flex justify-between items-center overflow-auto rounded-md bg-white border mt-8 p-3">
           <div class="flex items-center overflow-x-scoll cursor-pointer">
             <div
-              class="px-2 py-1 rounded font-medium capitalize whitespace-pre ml-2"
-              [routerLink]="['/shop/all']"
-              [routerLinkActive]="'active'">
-              All
+              *ngIf="loaderService.isLoading$ | async; else allCategories"
+              class="flex items-center h-10">
+              <div
+                *ngFor="let item of [0, 1, 2, 3, 4, 5]"
+                class="h-4 w-16 bg-black/10 rounded ml-6 animate-pulse"></div>
             </div>
+            <ng-template #allCategories>
+              <div
+                class="px-2 py-1 rounded font-medium capitalize whitespace-pre ml-2"
+                [routerLink]="['/shop/all']"
+                [routerLinkActive]="'active'">
+                All
+              </div>
+            </ng-template>
             <div
               *ngFor="let category of categories$ | async"
               class="px-2 py-1 rounded font-medium capitalize whitespace-pre ml-2"
@@ -31,9 +47,14 @@ import { ActivatedRoute, RouterModule } from '@angular/router';
             </div>
           </div>
         </div>
+        <div
+          *ngIf="loaderService.isLoading$ | async"
+          class="flex items-center justify-center h-screen">
+          <mat-spinner color="primary"></mat-spinner>
+        </div>
         <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 py-8">
           <app-product-card
-            *ngFor="let product of products$ | async"
+            *ngFor="let product of products$ | async; else: loading"
             [data]="product">
           </app-product-card>
         </div>
@@ -55,7 +76,8 @@ export class ProductsComponent implements OnInit {
 
   constructor(
     private productsService: ProductsService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    public loaderService: LoaderService
   ) {}
 
   ngOnInit() {
