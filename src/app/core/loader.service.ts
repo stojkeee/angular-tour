0609e4df0f6ac4b4d5b9a5ed6454520b65e9ba5a
stojic.mate <stojic.mate@nsoft.com>
2023-01-04 20:49:36 +0100

    create loader service

---
+++ src/app/core/loader.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoaderService {
+  private readonly _isLoading = new Subject<boolean>();
+
+  readonly isLoading$: Observable<boolean> = this._isLoading.asObservable();
+
+  loadingOn() {
+    this._isLoading.next(true);
+  }
+
+  loadingOff() {
+    this._isLoading.next(false);
+  }
+}
